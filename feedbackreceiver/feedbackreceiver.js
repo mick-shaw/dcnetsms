be1f48ddd22406990f610a91a667117fd53a6d6a
Mick <mshaw@potomacintegration.com>
2022-12-08 09:32:43 -0500

    Add files via upload

---
+++ feedbackreceiver/feedbackreceiver.js
@@ -0,0 +1,254 @@
+//xxxFeedBackReceiver.js
+//
+// Created by: Mick Shaw
+// Contact Information: voice.eng@dc.gov
+// Date: 12/07/2022
+// Subject: xxxxDFeedBackReceiver.js
+//
+// --------------------------------
+// Each time a recipient replies to an SMS, 
+// Amazon Pinpoint publishes the reply event to an Amazon SNS topic
+//  which is subscribed by an Amazon SQS queue. 
+// Amazon Pinpoint will also add a messageId to this event which allows 
+// us to bind it to a sendMessages operation call.
+
+// This AWS Lambda function polls these reply events from the Amazon SQS queue. 
+// It checks whether the reply is in the correct format (i.e. a number) and 
+// also associated with a previous request. If all conditions are met, 
+// the AWS Lambda function checks the ConversationStage attribute’s value 
+// from the message-lookup table. 
+// According to the current stage and the SMS answer received, 
+// AWS Lambda function will determine the next step.
+//  
+//
+// --------------------------------
+
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const pinpoint = new AWS.Pinpoint();
+const aws_region = "us-east-1";
+
+exports.handler = async (event) => {
+  console.log("Lambda triggered with event: " + JSON.stringify(event));
+  //Source of this lambda function is the SQS engine.
+  if(!event.Records) return "ERROR: Unexpected payload.";
+  let hasError=false;
+  for (const item of event.Records) {
+       try{
+         let body=JSON.parse(item.body);
+         let messageBody=body.messageBody;
+         let messageId=body.previousPublishedMessageId;
+         let destinationNumber=body.originationNumber;
+         //update the feedbacks table with the feedback number received.
+         var feedback = parseInt(messageBody);
+         if(!isNaN(feedback)){
+         	//determine the conversation stage. If this is the first feedback from the user, stage returns as 1.
+            let lookupData=await lookupAppointmentIdAndStage(messageId); // if messageid is not known throws an error. 
+         	let currentStage=lookupData.ConversationStage;
+         	let appointmentId=lookupData.AppointmentId;
+         	let message;
+         	let isConversationFinished=true;
+         	if(currentStage==1){
+            	//update the feedback score
+            	//send thank you sms
+	        	message="Please enter 1 for yes or 2 for no if you receive <Explanation of service> from DCNET";
+	            	currentStage=currentStage+1;
+	            	isConversationFinished=false;
+	           	await updateDynamoDbFeedbackScore1 (appointmentId, feedback);
+	                        }
+            else if(currentStage==2){
+                //update the feedback score
+            	//send thank you sms
+                message ="Please enter 1 for yes or 2 for no if you receive <frequency of service>";
+                	currentStage=currentStage+1;
+	            	isConversationFinished=false;
+	           	await updateDynamoDbFeedbackScore2 (appointmentId, feedback);
+	            
+            }
+            else if(currentStage==3){
+                //update the feedback score
+            	//send thank you sms
+	            message ="Please enter 1 for yes or 2 for no if you have any concerns about the services you are receiving from DCNET";
+	            	currentStage=currentStage+1;
+	            	isConversationFinished=false;
+	           await updateDynamoDbFeedbackScore3 (appointmentId, feedback);
+	            
+            }
+             else if(currentStage==4){
+                //send thank you sms
+	           await updateDynamoDbFeedbackScore4 (appointmentId, feedback);
+	            message ="Thank you for your feedback";
+	            	currentStage=currentStage+1;
+	            	isConversationFinished=true;
+	           
+            }
+            else
+            	throw ('unknown stage');
+
+            var smsParams = {
+	                        ApplicationId: process.env.APPLICATION_ID,
+	                        MessageRequest: {
+	                          Addresses: {
+	                            [destinationNumber]: {
+	                              ChannelType: 'SMS'
+	                            }
+	                          },
+	                          MessageConfiguration: {
+	                            SMSMessage: {
+	                              Body: message,
+	                              MessageType: 'TRANSACTIONAL'
+	                            }
+	                          }
+	                        }
+	            };
+
+            let result=await sendSMS(smsParams);
+            messageId=result['MessageResponse']['Result'][destinationNumber]['MessageId'];
+
+            if(isConversationFinished==false){
+            	//if conversation will continue, add the last messageid and the new stage to the lookup table
+            	//add this messageid to message-id order-id lookup table
+                    var params = {
+                                TableName: "message-lookup",
+                                ReturnConsumedCapacity: "TOTAL",
+                                Item: {
+                                    "FeedbackMessageId": messageId,
+                                    "AppointmentId": appointmentId,
+                                    "ConversationStage": currentStage
+                                }
+                    };
+                    result= await docClient.put(params).promise();
+            }
+        }//isNAN
+       }
+       catch(err){
+        //sms's which include non-numeric responses or which doesn't have corresponding feedbackmessageids will be ignored.
+         console.error(err.name, err.message);
+       }
+  }//for
+  return "OK";
+};//eventHandler
+
+
+async function lookupAppointmentIdAndStage(messageId){
+	try {
+			var params = {
+			  TableName : 'message-lookup',
+			  Key: {
+			    FeedbackMessageId: messageId
+			  }
+			};
+	    	const data = await docClient.get(params).promise();
+	    	return data.Item;
+	} catch (err) {
+	    console.log("Failure", err.message)
+	    throw err;
+	}
+}
+
+async function updateDynamoDbFeedbackScore1 (appointmentId, feedbackScore){
+	  const strAppintmentId=appointmentId.toString();
+	  let params = {
+	      TableName:'feedbacks',
+	      Key:{
+	          "AppointmentId": strAppintmentId
+	      },
+	      UpdateExpression: "set FeedbackScore = :val1",
+	      ConditionExpression: "attribute_exists(AppointmentId)",
+	      ExpressionAttributeValues:{
+	          ":val1":feedbackScore
+	      },
+	      ReturnValues:"UPDATED_NEW"
+	  };
+	  try {
+		    const data = await docClient.update(params).promise();
+		    return data;
+	 } catch (err) {
+		    console.log("Failure", err.message)
+		    throw err;
+	 }
+}
+
+
+async function updateDynamoDbFeedbackScore2 (appointmentId, feedbackScore){
+	  const strAppintmentId=appointmentId.toString();
+	  let params = {
+	      TableName:'feedbacks',
+	      Key:{
+	          "AppointmentId": strAppintmentId
+	      },
+	      UpdateExpression: "set FeedbackScore2 = :val1",
+	      ConditionExpression: "attribute_exists(AppointmentId)",
+	      ExpressionAttributeValues:{
+	          ":val1":feedbackScore
+	      },
+	      ReturnValues:"UPDATED_NEW"
+	  };
+	  try {
+		    const data = await docClient.update(params).promise();
+		    return data;
+	 } catch (err) {
+		    console.log("Failure", err.message)
+		    throw err;
+	 }
+}
+async function updateDynamoDbFeedbackScore3 (appointmentId, feedbackScore){
+	  const strAppintmentId=appointmentId.toString();
+	  let params = {
+	      TableName:'feedbacks',
+	      Key:{
+	          "AppointmentId": strAppintmentId
+	      },
+	      UpdateExpression: "set FeedbackScore3 = :val1",
+	      ConditionExpression: "attribute_exists(AppointmentId)",
+	      ExpressionAttributeValues:{
+	          ":val1":feedbackScore
+	      },
+	      ReturnValues:"UPDATED_NEW"
+	  };
+	  try {
+		    const data = await docClient.update(params).promise();
+		    return data;
+	 } catch (err) {
+		    console.log("Failure", err.message)
+		    throw err;
+	 }
+}
+
+async function updateDynamoDbFeedbackScore4 (appointmentId, feedbackScore){
+	  const strAppintmentId=appointmentId.toString();
+	  let params = {
+	      TableName:'feedbacks',
+	      Key:{
+	          "AppointmentId": strAppintmentId
+	      },
+	      UpdateExpression: "set FeedbackScore4 = :val1",
+	      ConditionExpression: "attribute_exists(AppointmentId)",
+	      ExpressionAttributeValues:{
+	          ":val1":feedbackScore
+	      },
+	      ReturnValues:"UPDATED_NEW"
+	  };
+	  try {
+		    const data = await docClient.update(params).promise();
+		    return data;
+	 } catch (err) {
+		    console.log("Failure", err.message)
+		    throw err;
+	 }
+}
+async function sendSMS (params) {
+    return new Promise ((resolve,reject) => {
+        pinpoint.sendMessages(params, function(err, data) {
+                      // If something goes wrong, print an error message.
+                      if(err) {
+                        console.log(err.message);
+                        reject(err);
+                      // Otherwise, show the unique ID for the message.
+                      } else {
+                        resolve(data);
+                      }
+        });
+ 
+    });
+}
